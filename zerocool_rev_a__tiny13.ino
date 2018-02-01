fd37a64bd6ac3d8caf5d290df9af5368f2dd5174
Chris <cms@dreaming.org>
2018-02-01 14:27:21 -0500

    PWM configuration and fan speed tests
    PWM frequency selected, suitable for driving 30^2mm 5V fan based on harmonics and impedance. PWM out values tested at Vcc of 4.8V. TODO: Values to be assigned to temperature ranges in a lookup table. Collect PWM output from thermal readings.

--- zerocool_rev_a__tiny13.ino
+++ zerocool_rev_a__tiny13.ino
@@ -3,8 +3,8 @@
  * Datasheet for ATtiny13: http://www.atmel.com/images/doc2535.pdf
  * 
  * Pin configuration -
- * PB0/OC0A: FAN p-chan FET output (Pin 5)
- * PB4/ADC2: NTC thermistor input (Pin 3)
+ * PB0/OC0A: FAN 4N25 gate driving pfet - output (Pin 5)
+ * PB4/ADC2: NTC thermistor interface - input (Pin 3)
  *
  * Based on original work at https://gist.github.com/adnbr/9289235
  * -------------------------------------------------------------------*/
@@ -12,7 +12,6 @@
 // 9.6 MHz, built in resonator
 #define F_CPU 9600000
 #define FAN PB0 
- 
  
 #include <avr/io.h>
  
@@ -28,39 +27,57 @@ void adc_setup (void)
     ADCSRA |= (1 << ADPS1) | (1 << ADPS0) | (1 << ADEN);
 }
  
-int adc_read (void)
+uint8_t adc_read (void)
 {
     // Start the conversion
     ADCSRA |= (1 << ADSC);
  
     // Wait for it to finish
     while (ADCSRA & (1 << ADSC));
- 
+    
+    // return the 8 bit value
     return ADCH;
 }
  
 void pwm_setup (void)
 {
-    // Set Timer 0 prescaler to clock/8.
-    // At 9.6 MHz this is 1.2 MHz.
+
+    // f_pwm = f_sysclock / Prescaler * Mode
+    // 9.6*10^6 / 64 * 256
+    // f_pwm = 586 Hz
+
+    // Set Timer 0 prescaler to clock/64 (150kHz). 
+    // P = 64
     // See ATtiny13 datasheet, Table 11.9.
-    TCCR0B |= (1 << CS01);
+    TCCR0B |= (1 << CS00) | (1 << CS01);
  
-    // Set to 'Fast PWM' mode
-    TCCR0A |= (1 << WGM01) | (1 << WGM00);
+    // Set to 'Fast PWM' mode - TOP value: 0xFF
+    // M = 256
+    // See ATtiny13 datasheet, Table 11.8.
+    TCCR0A |= (1 << WGM00) | (1 << WGM01);
  
-    // Clear OC0A output on compare match, upwards counting.
+    // Clear OC0A on Compare Match, set 0C0A at TOP
+    // See ATtiny13 datasheet, Table 11.3.
     TCCR0A |= (1 << COM0A1);
+
 }
  
-void pwm_write (int val)
+void pwm_write ( uint8_t val )
 {
     OCR0A = val;
+}
+
+void delay_nop( unsigned long delay ) 
+{
+  volatile unsigned long i = 0;
+  for (i = 0; i < delay; i++) {
+      __asm__ __volatile__ ("nop");
+  }
 }
  
 int main (void)
 {
-    int adc_in;
+    uint8_t adc_in;
  
     // FAN is an output.
     DDRB |= (1 << FAN);  
@@ -69,6 +86,17 @@ int main (void)
     pwm_setup();
   
     while (1) {
+
+        /*
+        Vcc = 4.8V
+        pwm_write((uint8_t)0x45); // 2V
+        pwm_write((uint8_t)0x75); // 2.7V
+        pwm_write((uint8_t)0xA5); // 3.5V
+        pwm_write((uint8_t)0xD5); // 4.1V
+        pwm_write((uint8_t)0xFE); // 4.5V
+        pwm_write((uint8_t)0x00); // 0V
+        */
+
         // Get the ADC value
         adc_in = adc_read();
         // Now write it to the PWM counter
